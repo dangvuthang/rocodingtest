b57cc29222ee3af62f71b69c93073e15fd680b58
Vu Thang <43143636+dangvuthang@users.noreply.github.com>
2022-03-24 18:57:50 +0700

    Revert "Feat/add test page"

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,9 +5,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider, EmotionCache } from "@emotion/react";
 import theme from "../src/theme";
 import createEmotionCache from "../src/createEmotionCache";
-import { PublicClientApplication } from "@azure/msal-browser";
-import { MsalProvider } from "@azure/msal-react";
-import { msalConfig } from "../src/authConfig";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -15,8 +12,8 @@ const clientSideEmotionCache = createEmotionCache();
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
+
 export default function MyApp(props: MyAppProps) {
-  const msalInstance = new PublicClientApplication(msalConfig);
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <CacheProvider value={emotionCache}>
@@ -26,9 +23,7 @@ export default function MyApp(props: MyAppProps) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <MsalProvider instance={msalInstance}>
-          <Component {...pageProps} />
-        </MsalProvider>
+        <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
   );
