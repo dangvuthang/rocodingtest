2074dd145a82466f8670974da196614bcbd4b651
Vu Thang <dangvuthang97@gmail.com>
2022-03-26 23:49:16 +0700

    Changed contect section to full-width

--- frontend/components/ContactSection.tsx
+++ frontend/components/ContactSection.tsx
@@ -4,25 +4,36 @@ import Image from "next/image";
 
 export default function ContactSection(): JSX.Element {
   return (
-    <Container maxWidth="md">
-      <Box sx={{ backgroundColor: "#fccc74", borderRadius: 4, p: 2 }}>
+    <Box sx={{ backgroundColor: "#fccc74", borderRadius: 4, p: 2 }}>
+      <Container maxWidth="md">
         <Grid container alignItems="center" spacing={4}>
-          <Grid item xs={12} sm={6} >
-            <Typography variant="h2"  sx={{ ml:6 }} >Contact us</Typography>
-            <Typography variant="body1" sx={{ ml:10, mb: 2 }}>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="h2" sx={{ ml: 6 }}>
+              Contact us
+            </Typography>
+            <Typography variant="body1" sx={{ ml: 10, mb: 2 }}>
               Get in touch to ask us anything, sales or otherwise!
             </Typography>
-            <Button sx={{ ml:18 }} variant="contained" href="mailto:hello@example.com">
+            <Button
+              sx={{ ml: 18 }}
+              variant="contained"
+              href="mailto:hello@example.com"
+            >
               Send an email
             </Button>
           </Grid>
           <Grid item xs={12} sm={6}>
             <Container>
-              <Image src="/assets/contact.svg" width={400} height={280} />
+              <Image
+                src="/assets/contact.svg"
+                width={400}
+                height={280}
+                alt="contact-image"
+              />
             </Container>
           </Grid>
         </Grid>
-      </Box>
-    </Container>
+      </Container>
+    </Box>
   );
 }
