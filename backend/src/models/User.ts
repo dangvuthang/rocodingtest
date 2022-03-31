9d153c9d41facaa6c8813bc9146afeed566a5e6c
Vu Thang <dangvuthang97@gmail.com>
2022-03-31 18:17:56 +0700

    Add index to mongodb email field for faster lookup time

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,25 +1,25 @@
-import mongoose, { Schema} from 'mongoose';
+import mongoose, { Schema } from "mongoose";
 
 export interface IUser {
-    fullName: string;
-    email: string;
-    photoUrl: string;
-    roles: string;
-  }
+  fullName: string;
+  email: string;
+  photoUrl: string;
+  roles: string;
+}
 
-  const UserSchema: Schema = new Schema({
-    fullName: {type: String, required: true},
-    email: {type: String, required: true},
-    photoUrl: {type: String, required: true},
-    role: {
-      type: String,
-      enum: {
-        values: ["student", "teacher"],
-        message: "Role must be student, or teacher",
-      },
-      default: "student",
-    }
-  });
+const UserSchema: Schema = new Schema({
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, index: true },
+  photoUrl: { type: String, required: true },
+  role: {
+    type: String,
+    enum: {
+      values: ["student", "teacher"],
+      message: "Role must be student, or teacher",
+    },
+    default: "student",
+  },
+});
 
-  // Export the model and return your IUser interface
-  export default mongoose.model<IUser>('User', UserSchema);
+// Export the model and return your IUser interface
+export default mongoose.model<IUser>("User", UserSchema);
