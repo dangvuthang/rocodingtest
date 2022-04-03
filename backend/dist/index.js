1d378a6fbf4332e71ec033d95b1d9cca83c66fe9
Vu Thang <dangvuthang97@gmail.com>
2022-04-03 14:55:32 +0700

    Handle cors for backend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const server_1 = __importDefault(require("./server"));
+const cors_1 = __importDefault(require("cors"));
 const UserRoute_1 = __importDefault(require("./route/UserRoute"));
 const app = (0, express_1.default)();
 const port = 8080;
+app.use((0, cors_1.default)({ origin: "http://localhost:3000" }));
 app.use(express_1.default.json());
 app.get("/", (_, res) => res.send("Welcome to the Mongoose & TypeScript example"));
 app.use("/api/v1/users", UserRoute_1.default);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,12 @@
 import express, { Response, Application } from "express";
 import server from "./server";
+import cors from "cors";
 import UserRoute from "./route/UserRoute";
 const app: Application = express();
 const port = 8080;
 
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
-
 app.get("/", (_, res: Response) =>
   res.send("Welcome to the Mongoose & TypeScript example")
 );
