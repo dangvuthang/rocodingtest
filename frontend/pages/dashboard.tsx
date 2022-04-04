9c1dfa672bd48feba8f9b9a503f4c0db099c737e
tuanhoang-hub <s3817872@rmit.edu.vn>
2022-04-04 16:26:08 +0700

    nothing-new

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,65 +1,117 @@
-import { Box,Button, Grid, IconButton, Typography, Paper } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
-import CheckBoxIcon from '@mui/icons-material/CheckBox';
-import * as React from 'react';
-import Dslayout from '../components/dashboard/Dslayout/Dslayout';
+import {
+  Box,
+  Button,
+  Grid,
+  IconButton,
+  Typography,
+  Paper,
+  Divider,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import * as React from "react";
+import Dslayout from "../components/dashboard/Dslayout/Dslayout";
 
-export default function Dashboard()  {
-    const [check, setCheck] = React.useState(false);
-    const handleCheckBox = () => {
-        setCheck(!check);
-        console.log(check);
-      }
-    return(
+export default function Dashboard() {
+  const [check, setCheck] = React.useState(false);
+  const handleCheckBox = () => {
+    setCheck(!check);
+    console.log(check);
+  };
+  return (
     <Dslayout>
-        <Grid >
+      <Grid>
         <Grid item>
-          <Box width='98%' sx=
-            {{
+          <Box
+            width="98%"
+            sx={{
               py: 1.5,
               mx: 2,
               my: 2,
-              bgcolor: 'white',
+              bgcolor: "white",
               borderRadius: 3.5,
-            }}>
-            <Button variant="contained" startIcon={<AddIcon />} sx={{ ml: 2 }}>Add exam</Button>
-            <Button variant="contained" sx={{ ml: 2 }}>Weekly Details</Button>
-            <Button variant="contained" sx={{ ml: 2 }}>Move to category</Button>
-            <Button variant="contained" sx={{ ml: 2 }}>Move to archiev</Button>
+            }}
+          >
+            <Grid container direction="row" justifyContent="space-around">
+              <Grid item>
+                <Button variant="contained" startIcon={<AddIcon />}>
+                  Add exam
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button variant="contained">Weekly Details</Button>
+              </Grid>
+              <Grid item>
+                <Button variant="contained">Move to category</Button>
+              </Grid>
+              <Grid item>
+                <Button variant="contained">Move to archiev</Button>
+              </Grid>
+            </Grid>
           </Box>
         </Grid>
         <Grid item>
-          <Box width='98%' sx=
-            {{
+          <Box
+            width="98%"
+            sx={{
               py: 1,
               mx: 2,
               my: 2,
-              bgcolor: 'white',
+              bgcolor: "white",
               borderRadius: 3.5,
-              height: '80vh',
-            }}>
+              height: "80vh",
+            }}
+          >
             {/* Page Content */}
-            <Grid container spacing={2} direction="column"
+            <Grid
+              container
+              spacing={2}
+              direction="column"
               justifyContent="center"
-              alignItems="center">
-              <Grid item sx={{ width: '90%' }}>
+              alignItems="center"
+            >
+              <Grid item sx={{ width: "90%" }}>
                 <Box mb={3}>
                   <Paper>
                     <Box p={2}>
-                      <Grid container alignItems="center" spacing={1}>
-                        {check === false ?
-                          <IconButton onClick={handleCheckBox}>
-                            <CheckBoxOutlineBlankIcon />
-                          </IconButton> :
-                          <IconButton onClick={handleCheckBox}>
-                            <CheckBoxIcon />
-                          </IconButton>}
+                      <Grid
+                        container
+                        direction="row"
+                        alignItems="center"
+                        spacing={1}
+                      >
                         <Grid item>
-                          <img src="https://picsum.photos/200/300" alt="hehe" width="100%" />
+                          {check === false ? (
+                            <IconButton onClick={handleCheckBox}>
+                              <CheckBoxOutlineBlankIcon />
+                            </IconButton>
+                          ) : (
+                            <IconButton onClick={handleCheckBox}>
+                              <CheckBoxIcon />
+                            </IconButton>
+                          )}
                         </Grid>
                         <Grid item>
-                          <Typography>hehe</Typography>
+                          <img
+                            src="https://picsum.photos/200/200"
+                            alt="hehe"
+                            width="100%"
+                          />
+                        </Grid>
+                        <Grid item>
+                          <Grid container direction="column" spacing={6}>
+                            <Grid item>
+                              <Typography variant="h2">Test</Typography>
+                            </Grid>
+                            <Grid item>
+                              <Typography variant="body2">hehe </Typography>
+                              <Typography variant="body2">hehe</Typography>
+                            </Grid>
+                            <Grid item>
+                              <Typography variant="body2">hehe</Typography>
+                            </Grid>
+                          </Grid>
                         </Grid>
                       </Grid>
                     </Box>
@@ -71,5 +123,5 @@ export default function Dashboard()  {
         </Grid>
       </Grid>
     </Dslayout>
-    );
+  );
 }
