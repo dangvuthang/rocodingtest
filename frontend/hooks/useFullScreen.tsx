b12e0bce9c3cf271705575dcdd1c44b0ab64671d
Vu Thang <dangvuthang97@gmail.com>
2022-04-05 07:42:33 +0700

    Add custom hook to detect whether the website is displayed as fullscreen

---
+++ frontend/hooks/useFullScreen.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+const useFullScreen = () => {
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  useEffect(() => {
+    const handleFullScreenChange = (e: Event) => {
+      console.log("ENTER FULLSCREEN CHANGED");
+      if (document.fullscreenElement) setIsFullScreen(true);
+      else setIsFullScreen(false);
+    };
+    document.addEventListener("fullscreenchange", handleFullScreenChange);
+    return () =>
+      document.removeEventListener("fullscreenchange", handleFullScreenChange);
+  }, []);
+  return isFullScreen;
+};
+
+export default useFullScreen;
