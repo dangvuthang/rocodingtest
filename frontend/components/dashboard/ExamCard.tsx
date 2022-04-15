a101f5676c538a007af1f8efae8c02fabeaa09d5
nhannguyentruc214 <89124813+nhannguyentruc214@users.noreply.github.com>
2022-04-15 20:55:30 +0700

    update

--- frontend/components/dashboard/ExamCard.tsx
+++ frontend/components/dashboard/ExamCard.tsx
@@ -1,7 +1,3 @@
-import { Grid,Box,Paper,IconButton,Typography } from '@mui/material'
-import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
-import CheckBoxIcon from "@mui/icons-material/CheckBox";
-import DeleteIcon from "@mui/icons-material/Delete";
 import CreatedTests from "../interfaces/CreatedTests";
 import * as React from 'react'
 
@@ -12,15 +8,6 @@ type Property = {
 }
 
 const ExamCard: React.FC<Property> =  ({ prop , deleteExam, editRow }) => {
-    const [check, setCheck]: any = React.useState([]);
-    const handleCheckBox = (index: any) => {
-        const foundExam = check.find((x: any) => x === index);
-        if (foundExam >= 0) {
-        setCheck(check.filter((x: any) => x !== foundExam));
-        } else {
-        setCheck([...check, index]);
-        }
-    };
   return (
       <div key={prop.exam_id} className="mt-4 ml-4 mr-4 h-32  border border-black flex gap-3 items-center">
         <div className="w-10">
@@ -55,101 +42,3 @@ const ExamCard: React.FC<Property> =  ({ prop , deleteExam, editRow }) => {
 }
 
 export default ExamCard
-
-{/*
-  <Grid item sx={{ width: "90%" }} key={prop.exam_id} mb={2}>
-                  <Box mt={3}>
-                    <Paper elevation={8}>
-                      <Box p={2}>
-                        <Grid container spacing={1}>
-                          <Grid
-                            item
-                            xs={1}
-                            sx={{ display: "flex" }}
-                            alignItems="center"
-                          >
-                            {!check.includes(prop.exam_id) ? (
-                              <IconButton onClick={() => handleCheckBox(prop.exam_id)}>
-                                <CheckBoxOutlineBlankIcon />
-                              </IconButton>
-                            ) : (
-                              <IconButton onClick={() => handleCheckBox(prop.exam_id)}>
-                                <CheckBoxIcon />
-                              </IconButton>
-                            )}
-                          </Grid>
-                          <Grid item xs={2}>
-                            <img
-                              src="https://picsum.photos/200/200"
-                              alt="hehe"
-                              width="100%"
-                              height="250px"
-                            />
-                          </Grid>
-                          <Grid item xs={5}>
-                            <Grid
-                              container
-                              direction="column"
-                              justifyContent="space-between"
-                              sx={{ height: "100%" }}
-                            >
-                              <Grid item>
-                                <Typography variant="h2" textAlign="left" p={1}>
-                                    {prop.name}
-                                </Typography>
-                              </Grid>
-                              <Grid item>
-                                <Box
-                                  sx={{ display: "flex", alignItems: "center" }}
-                                >
-                                  <Typography variant="body2" p={1}>
-                                    Defaut Category{" "}
-                                  </Typography>
-                                  <Typography>|</Typography>
-                                  <Typography variant="body2" p={1}>
-                                    huhu{" "}
-                                  </Typography>
-                                </Box>
-                              </Grid>
-                              <Grid item>
-                                <Box
-                                  sx={{ display: "flex", alignItems: "center" }}
-                                >
-                                  <Typography variant="body2" p={1}>
-                                    {prop.question}
-                                  </Typography>
-                                  <Typography variant="body2" p={1}>
-                                    Created Time: 28/02/2022
-                                  </Typography>
-                                </Box>
-                              </Grid>
-                            </Grid>
-                          </Grid>
-                          <Grid
-                            item
-                            xs={4}
-                            sx={{ display: "flex" }}
-                            justifyContent="flex-end"
-                            alignItems="center"
-                          >
-                            <IconButton>
-                              <DeleteIcon 
-                              onClick={ () =>{
-                                deleteExam(prop.exam_id)
-                              }
-                              }/>
-                            </IconButton>
-                            <IconButton>
-                              <EditIcon 
-                             onClick={ () =>{
-                              editRow(prop)
-                            }}
-                              />
-                            </IconButton>
-                          </Grid>
-                        </Grid>
-                      </Box>
-                    </Paper>
-                  </Box>
-                </Grid>
-  */}
--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,18 +1,5 @@
-import {
-  Box,
-  Button,
-  Grid,
-  IconButton,
-  Typography,
-  Paper,
-  Divider,
-} from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
-import CheckBoxIcon from "@mui/icons-material/CheckBox";
-import DeleteIcon from "@mui/icons-material/Delete";
+
 import * as React from "react";
-import Dslayout from "../components/dashboard/Ds_layout/Dslayout";
 import ExamCard from "../components/dashboard/ExamCard";
 import Link from "next/link";
 import AddExam from "../components/dashboard/AddExam";
@@ -20,129 +7,108 @@ import CreatedTests from "../components/interfaces/CreatedTests";
 import EditExam from "../components/dashboard/EditExam";
 import { getRequest } from "../util/axiosInstance";
 import { useEffect } from "react";
+import Header from "../components/layout/Header";
+import { Typography } from "@mui/material";
+import NavDrawer from "../components/dashboard/Ds_layout/NavDrawer";
 
 export default function Dashboard() {
 
-  const [tests, setTests] = React.useState<CreatedTests[]>([])
+    const [tests, setTests] = React.useState<CreatedTests[]>([])
 
-  const [currentUser, setCurrentUser] = React.useState<CreatedTests | {}>()
-  const [editing, setEditing] = React.useState(false)
-  const [adding, setAdding] = React.useState(false)
-
-  useEffect(() => {
-    const testData = getRequest({ url: "/users/60f6ce0e02f5102cea240400/tests" })
-    testData.then(result => {
-      setTests(result.data.data.tests)
-    }).catch(err => {
-      console.log(err)
-    })
-  }, [])
-  console.log(tests);
-
-  const addExam = async (e: React.FormEvent, formData: CreatedTests) => {
-    e.preventDefault()
-    const test: CreatedTests = {
-      exam_id: Math.random(),
-      _id: formData._id,
-      name: formData.name,
-      question: formData.question,
+    const [currentUser, setCurrentUser] = React.useState<CreatedTests | {}>()
+    const [editing, setEditing] = React.useState(false)
+    const [adding, setAdding] = React.useState(false)
+  
+    useEffect(() => {
+      const testData = getRequest({ url: "/users/60f6ce0e02f5102cea240400/tests" })
+      testData.then(result => {
+        setTests(result.data.data.tests)
+      }).catch(err => {
+        console.log(err)
+      })
+    }, [])
+    console.log(tests);
+  
+    const addExam = async (e: React.FormEvent, formData: CreatedTests) => {
+      e.preventDefault()
+      const test: CreatedTests = {
+        exam_id: Math.random(),
+        _id: formData._id,
+        name: formData.name,
+        question: formData.question,
+      }
+      setTests([...tests, test])
     }
-    setTests([...tests, test])
-  }
+  
+    const deleteExam = async (deleteId: number) => {
+      const exams: CreatedTests[] = tests.filter((exam: CreatedTests) => exam.exam_id !== deleteId)
+      console.log(exams)
+      setTests(exams)
+    }
+    const updateExam = (exam_id: number, updatedTest: any) => {
+  
+          setTests(tests.map(test => (test.exam_id === exam_id ? updatedTest : test)))
+      }
+  
+    const editRow = async (test: any) => {
+      setEditing(true)
+          setCurrentUser({ exam_id: test.exam_id, name: test.name, question: test.question, })
+      console.log(currentUser)
+    }
 
-  const deleteExam = async (deleteId: number) => {
-    const exams: CreatedTests[] = tests.filter((exam: CreatedTests) => exam.exam_id !== deleteId)
-    console.log(exams)
-    setTests(exams)
-  }
-  const updateExam = (exam_id: number, updatedTest: any) => {
+    function Copyright(props: any) {
+        return (
+            <footer className="w-full h-20 static bottom-0 mt-4 flex justify-center items-center">
+                <Typography variant="body2" color="text.secondary" align="center" {...props}>
+                    {'Copyright © '}
+                    <Link href="https://mui.com/">
+                        Your Website
+                    </Link>{' '}
+                    {new Date().getFullYear()}
+                    {'.'}
+                </Typography>
+            </footer>
+        );
+    }
 
-		setTests(tests.map(test => (test.exam_id === exam_id ? updatedTest : test)))
-	}
 
-  const editRow = async (test: any) => {
-    setEditing(true)
-		setCurrentUser({ exam_id: test.exam_id, name: test.name, question: test.question, })
-    console.log(currentUser)
-  }
-
-  return (
-    <Dslayout>
-        <Grid>
-          <Box
-            width="98%"
-            sx={{
-              py: 1,
-              mx: 1,
-              my: 1,
-              bgcolor: "white",
-              borderRadius: 3.5,
-            }}
-          >
-            {/* Page Content */}
-            {
-
+    return (
+        <NavDrawer>
+            {   
+                    editing ? 
+                (      
+                    <EditExam setEditing={setEditing} updateExam={updateExam} currentUser={currentUser}/>
+                ) 
+                    : adding ? 
+                (      
+                    <AddExam setAdding={setAdding} saveExam={addExam} />
+                ) 
+                    : 
+                (
+                    <div className="w-full">
+                        <div className="h-20 grid grid-cols-4 content-center justify-items-center border-l border-b border-black">
+                            <div>
+                                <button onClick={ () => setAdding(true) }className="ml-5 px-4 py-2 text-white rounded-md border-solid border bg-blue-400">
+                                    Add Exam
+                                </button>
+                            </div>
+                        </div>
+                        {/* Exam area */}
+                        <div className="h-full">
+                                {tests.length > 0 ? (
+                                    tests.map(test => (
+                                        <ExamCard editRow={editRow} prop={test} deleteExam={deleteExam} />
+                                    ))
+                                ) : (
+                                <tr>
+                                    <td colSpan={3}>No users</td>
+                                </tr>
+                                )}
+                        </div>
+                    </div>
+                )
             }
-            {/*Toggle between Add Page, Edit Page and Test Created List*/}
-            {
-                editing ? 
-              (      
-                <EditExam setEditing={setEditing} updateExam={updateExam} currentUser={currentUser}/>
-              ) 
-                : adding ? 
-              (      
-                <AddExam setAdding={setAdding} saveExam={addExam} />
-              ) 
-                : 
-              (
-                <Grid>
-                    <Grid item>
-                      <Box
-                        width="98%"
-                        sx={{
-                          py: 1.5,
-                          mx: 2,
-                          my: 2,
-                          bgcolor: "white",
-                          borderRadius: 3.5,
-                        }}
-                      >
-                        <Grid container direction="row" justifyContent="space-around">
-                          <Grid item>
-                            <Button
-                              variant="contained"
-                              startIcon={<AddIcon sx={{ color: "white" }} />}
-                              onClick={() => { setAdding(true); } }
-                            >
-                              Add exam
-                            </Button>
-                          </Grid>
-                        </Grid>
-                      </Box>
-                    </Grid>
-
-                    <Grid
-                      container
-                      direction="column"
-                      justifyContent="center"
-                      alignItems="center"
-                    >
-                        {tests.length > 0 ? (
-                          tests.map(test => (
-                            <ExamCard editRow={editRow} prop={test} deleteExam={deleteExam} />
-                          ))
-                        ) : (
-                          <tr>
-                            <td colSpan={3}>No users</td>
-                          </tr>
-                        )}
-                      </Grid>
-                </Grid>
-              )
-            }
-            {/* exam 2 */}
-          </Box>
-        </Grid>
-    </Dslayout>
-  );
+            <Copyright />
+        </NavDrawer>
+    );
 }
