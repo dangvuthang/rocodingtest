6765aecb09835ee16383ef379a7495e21823328d
Vu Thang <dangvuthang97@gmail.com>
2022-04-21 15:19:39 +0700

    integrate with backend API

--- frontend/components/ExamContent.tsx
+++ frontend/components/ExamContent.tsx
@@ -1,5 +1,4 @@
-import { FC, useEffect, useReducer, useRef, useState } from "react";
-import { GetStaticPaths } from "next";
+import { FC, useCallback, useEffect, useRef, useState } from "react";
 import Router from "next/router";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 import CodeArea from "./CodeArea";
@@ -11,31 +10,125 @@ import useFullScreen from "../hooks/useFullScreen";
 import useWindowFocus from "../hooks/useWindowFocus";
 import GlanceTracker from "./GlanceTracker";
 import { Test } from "../pages/exam/[id]";
+import { patchRequest, postRequest } from "../util/axiosInstance";
+import useAccessToken from "../hooks/useAccessToken";
+import { captureScreen, savedToCloudinary } from "../util/captureScreen";
+import ConfirmModal from "./ConfirmModal";
+import LoadingModal from "./LoadingModal";
 
 interface ExamContentProps {
   test: Test;
+  onDone: () => void;
 }
 
-const ExamContent: FC<ExamContentProps> = ({ test }) => {
+const ExamContent: FC<ExamContentProps> = ({ test, onDone }) => {
   const [run, setRun] = useState(false);
+  const [language, setLanguage] = useState("javascript");
+  const [content, setContent] = useState("");
+  const [runMessage, setRunMessage] = useState("");
   const [showInstruction, setShowInstruction] = useState(true);
   const [showAlertMessage, setShowAlertMessage] = useState(false);
+  const [showSubmissionConfirm, setShowSubmissionConfirm] = useState(false);
+  const [recordId, setRecordId] = useState("");
+  const [loading, setLoading] = useState(false);
   const [remainingTime, setRemainingTime] = useState(3);
   const [alertMessage, setAlertMessage] = useState("");
   const [isWatching, setIsWatching] = useState(true);
+
+  const accessToken = useAccessToken();
   const isFullscreen = useFullScreen();
   const isFocused = useWindowFocus();
   const count = useRef(0);
 
-  const handleCloseWarning = async () => {
-    if (!isFullscreen) {
-      try {
-        await document.documentElement.requestFullscreen();
-      } catch (error) {
-        console.log(error);
+  const handleStartExam = async () => {
+    setShowInstruction(false);
+    try {
+      const request = await postRequest({
+        url: "/records",
+        token: accessToken,
+        body: {
+          testId: Router.query.id,
+        },
+      });
+      const recordId = request.data.data.record._id;
+      setRecordId(recordId);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleRunCode = async () => {
+    setRun(true);
+    if (!content) {
+      setRunMessage("> Nothing to compile...");
+      return;
+    }
+    setRunMessage(`> Compiling...`);
+    try {
+      const request = await postRequest({
+        url: "/compile",
+        token: accessToken,
+        body: {
+          language,
+          source: content,
+        },
+      });
+      const message = request.data.data.output;
+      setRunMessage(`> ${message}`);
+    } catch (error) {
+      if ((error as any)!.response!.data!.msg) {
+        setRunMessage((error as any)!.response!.data!.msg);
       }
     }
+  };
+
+  const handleSubmitCode = useCallback(async () => {
+    if (recordId) {
+      setShowSubmissionConfirm(false);
+      setLoading(true);
+      let request;
+      try {
+        request = await postRequest({
+          url: `/tests/${test._id}/submissions`,
+          token: accessToken,
+          body: { recordId: recordId, content: content },
+        });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+        console.log(request);
+        if (request?.data?.status === "success") {
+          if (isFullscreen) {
+            await document.exitFullscreen().catch((err) => console.log(err));
+          }
+          onDone();
+        }
+      }
+    }
+  }, [accessToken, content, isFullscreen, onDone, recordId, test._id]);
+
+  const handleCloseWarning = async () => {
+    const image = await captureScreen().catch((err) => console.log(err));
     setShowAlertMessage(false);
+    if (!isFullscreen && remainingTime !== 0) {
+      await document.documentElement
+        .requestFullscreen()
+        .catch((err) => console.log(err));
+    }
+    let evidence;
+    if (image) {
+      evidence = await savedToCloudinary(image).catch((err) =>
+        console.log(err)
+      );
+    }
+    if (recordId) {
+      await patchRequest({
+        url: `/records/${recordId}`,
+        body: { evidence },
+        token: accessToken,
+      }).catch((err) => console.log(err));
+    }
   };
 
   // Detect navigate outside webste
@@ -44,7 +137,7 @@ const ExamContent: FC<ExamContentProps> = ({ test }) => {
     if (!isFocused) {
       setAlertMessage("you are trying to navigate outside of the web page");
       setShowAlertMessage(true);
-      setRemainingTime((time) => time - 1);
+      setRemainingTime((time) => (time > 0 ? time - 1 : 0));
     }
   }, [isFocused, showInstruction]);
 
@@ -56,42 +149,44 @@ const ExamContent: FC<ExamContentProps> = ({ test }) => {
       if (count.current === 1) return;
       setAlertMessage("you are trying to minimize the web page");
       setShowAlertMessage(true);
-      setRemainingTime((time) => time - 1);
+      setRemainingTime((time) => (time > 0 ? time - 1 : 0));
     }
   }, [isFullscreen, showInstruction]);
 
   // Detect glance tracking
-  useEffect(() => {
+  /*   useEffect(() => {
     if (showInstruction) return;
     if (!isWatching) {
       setAlertMessage("you are not looking at the screen");
       setShowAlertMessage(true);
       setRemainingTime((time) => time - 1);
     }
-  }, [isWatching, showInstruction]);
+  }, [isWatching, showInstruction]); */
 
   useEffect(() => {
-    console.log(remainingTime);
-    if (remainingTime === 0) {
-      Router.push("/")
-        .then(() => document.exitFullscreen())
-        .then(() => console.log("DONE"))
-        .catch((err) => console.log(err));
+    if (remainingTime === 0 && showAlertMessage === false) {
+      handleSubmitCode();
     }
-  }, [remainingTime]);
+  }, [handleSubmitCode, remainingTime, showAlertMessage]);
 
   return (
     <>
-      <GlanceTracker onChange={setIsWatching} />
+      {/* <GlanceTracker onChange={setIsWatching} /> */}
+      <LoadingModal open={loading} />
       <InstructionModal
         open={showInstruction}
-        onClose={() => setShowInstruction(false)}
+        onClose={() => handleStartExam()}
       />
       <AlertModal
         message={alertMessage}
         open={showAlertMessage}
         remainingTime={remainingTime}
         onClose={handleCloseWarning}
+      />
+      <ConfirmModal
+        open={showSubmissionConfirm}
+        onClose={() => setShowSubmissionConfirm(false)}
+        onConfirm={handleSubmitCode}
       />
       <div className="grid grid-cols-[1fr_3fr] h-screen grid-rows-[4%_4fr_auto]">
         <div className="col-span-2 justify-self-end self-end px-2">
@@ -102,7 +197,12 @@ const ExamContent: FC<ExamContentProps> = ({ test }) => {
         </div>
         <div className="relative">
           <div className="h-full">
-            <CodeArea />
+            <CodeArea
+              language={language}
+              content={content}
+              setContent={setContent}
+              setLanguage={setLanguage}
+            />
           </div>
           <div
             className={`absolute bottom-0 left-0 w-full ${!run && "hidden"}`}
@@ -119,8 +219,8 @@ const ExamContent: FC<ExamContentProps> = ({ test }) => {
               </button>
             </div>
             <div className="bg-white flex justify-center items-center h-32">
-              <div className="w-[90%] h-[80%] bg-black border border-transparent text-white px-1 font-mono">
-                {"> Compiling......"}
+              <div className="w-[90%] h-[80%] bg-black border border-transparent text-white px-1 font-mono overflow-y-auto">
+                {runMessage}
               </div>
             </div>
           </div>
@@ -129,11 +229,14 @@ const ExamContent: FC<ExamContentProps> = ({ test }) => {
           <div className="flex justify-end items-center pr-8">
             <button
               className="rounded-sm px-4 py-3 border border-[#b0bec5] text-[#455a64] leading-5 bg-transparent min-w-[80px] flex justify-center items-center h-[32px] text-sm hover:border-[#263238] hover:text-[#263238] hover:bg-[#eceff1] transition mr-5"
-              onClick={() => setRun(true)}
+              onClick={handleRunCode}
             >
               Run code
             </button>
-            <button className="shadow-sm rounded-sm px-4 py-3 text-white leading-5 bg-[#455a64] min-w-[80px] flex justify-center items-center h-[32px] text-sm hover:text-white hover:bg-[#546e7a] transition">
+            <button
+              className="shadow-sm rounded-sm px-4 py-3 text-white leading-5 bg-[#455a64] min-w-[80px] flex justify-center items-center h-[32px] text-sm hover:text-white hover:bg-[#546e7a] transition"
+              onClick={() => setShowSubmissionConfirm(true)}
+            >
               Submit
             </button>
           </div>
