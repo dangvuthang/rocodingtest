8e7858002927fad38f2add1725a232b683be8920
Vu Thang <dangvuthang97@gmail.com>
2022-05-12 16:28:40 +0700

    Delegate all model loading to LoadScript components

--- frontend/components/LoadScript.tsx
+++ frontend/components/LoadScript.tsx
@@ -1,17 +1,25 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import Script from "next/script";
 
 const LoadScript: FC = () => {
+  useEffect(() => {
+    const faceapi = (window as any).faceapi;
+    const initializeModel = async () => {
+      await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
+      await faceapi.nets.faceLandmark68TinyNet.loadFromUri("/models");
+      await faceapi.nets.faceRecognitionNet.loadFromUri("/models");
+    };
+    if (faceapi) {
+      initializeModel();
+    }
+  }, []);
   return (
     <>
       <Script
         src="/jeelizGlanceTracker.js"
         strategy="beforeInteractive"
       ></Script>
-      <Script
-        src="/face-api.js"
-        strategy="beforeInteractive"
-      ></Script>
+      <Script src="/face-api.js" strategy="beforeInteractive"></Script>
     </>
   );
 };
